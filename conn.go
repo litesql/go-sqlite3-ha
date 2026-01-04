50be5e961f1e5d90656c81342ad6dc6ce749f5f2
Walter Wanderley <walterwanderley@gmail.com>
2026-01-04 20:43:29 -0300

    fix: exit stream goroutines on error

--- conn.go
+++ conn.go
@@ -332,6 +332,7 @@ func (c *Conn) start() error {
 				c.currentRedirectTarget = ""
 				slog.Debug("failed to receive message", "error", err)
 				c.Close()
+				return
 			}
 			c.resCh <- msg
 		}
@@ -344,6 +345,7 @@ func (c *Conn) start() error {
 				c.currentRedirectTarget = ""
 				slog.Debug("failed to send message", "error", err)
 				c.Close()
+				return
 			}
 		}
 	}()
